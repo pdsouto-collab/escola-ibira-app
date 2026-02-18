5d9d9c21273ef95245fd3b2be1f3a71a0058a565
pdsouto-collab <pdsouto@gmail.com>
2026-02-18 10:32:50 -0300

    feat: use SchoolLogo component with left alignment and larger size

--- src/components/dashboard/hero.tsx
+++ src/components/dashboard/hero.tsx
@@ -2,6 +2,7 @@
 
 import { Bell, Search } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { SchoolLogo } from "../ui/school-logo";
 
 import { useAppStore } from "@/lib/store";
 
@@ -37,13 +38,8 @@ export function DashboardHero() {
                 </div>
 
                 {/* Tree Illustration */}
-                <div className="hidden md:block absolute right-0 bottom-0 opacity-20 md:opacity-100 md:relative md:w-64 md:h-64 translate-y-6 md:translate-y-0 text-white">
-                    {/* eslint-disable-next-line @next/next/no-img-element */}
-                    <img
-                        src="/escola-ibira-app/images/logo-ibira-final.png"
-                        alt="Escola Ibirá"
-                        className="w-full h-full object-contain mix-blend-multiply opacity-90"
-                    />
+                <div className="hidden md:block absolute right-0 bottom-0 opacity-20 md:opacity-100 md:relative md:w-80 md:h-80 translate-y-6 md:translate-y-0 text-white">
+                    <SchoolLogo className="w-full h-full" variant="light" />
                 </div>
             </div>
         </div>
