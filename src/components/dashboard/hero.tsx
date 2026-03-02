6501feed62fa947ffb7b7a70514c2f7e1eb6577c
pdsouto-collab <pdsouto@gmail.com>
2026-03-02 16:27:01 -0300

    style: use transparent PNG for dashboard logo

--- src/components/dashboard/hero.tsx
+++ src/components/dashboard/hero.tsx
@@ -40,7 +40,7 @@ export function DashboardHero() {
                 <div className="hidden md:block absolute right-0 bottom-0 opacity-20 md:opacity-100 md:relative md:w-56 md:h-56 translate-y-8 translate-x-4 md:translate-y-6 md:translate-x-0">
                     {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img
-                        src="/escola-ibira-app/images/logo-trilha.svg"
+                        src="/escola-ibira-app/images/opcao2_v5_transparent.png"
                         alt="Escola Ibirá"
                         className="w-full h-full object-contain origin-bottom-right"
                     />
