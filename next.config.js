463440771226221eeb9fbadfe72d0e1fde84fd82
Carlos Henrique Ferreira Gomes <carlos.henriquefg@gmail.com>
2026-03-10 16:44:52 -0300

    Ajustando o arquivo next.config para vercel

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    basePath: "/escola-ibira-app",
-    trailingSlash: true,
-    images: { unoptimized: true },
-    generateEtags: false,
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
