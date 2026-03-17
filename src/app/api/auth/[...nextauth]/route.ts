2164476029e02efdfa92e03f77c41a8a3bf4474b
Carlos Henrique Ferreira Gomes <carlos.henriquefg@gmail.com>
2026-03-17 17:59:50 -0300

    Alterando o nome da variavel de ambiente

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -75,7 +75,7 @@ export const authOptions: NextAuthOptions = {
     pages: {
         signIn: "/login",
     },
-    secret: process.env.AUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
 };
 
 const handler = NextAuth(authOptions);
